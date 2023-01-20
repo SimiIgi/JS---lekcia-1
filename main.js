a05b6826d710824c8aa7cdd53cbdf54a05b0f3fd
“Simona <simona.ignacz@gmail.com>
2023-01-20 11:00:12 +0100

    uprava

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+function zobraz_vysledok() {
+  let vysledok = document.querySelector("#vysledek");
+  vysledok.innerHTML = secti(4, 5);
+
+}
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -8,6 +12,8 @@ document.querySelector("#vysledek").innerHTML = secti(4, 5);
  * @param {int} a
  * @param {int} b
  */
+
+
 function secti(a, b) {
   let c = a + b;
   return c;
@@ -18,4 +24,16 @@ function secti(a, b) {
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
+
+  let text_stvorec = document.querySelector(".ctverecek");
+  text_stvorec.innerHTML = "Gratulace, právě jsi spustila tuto funkci!";
+  text_stvorec.style.lineHeight = "20px"
 }
+
+function farba_stvorceka() {
+  let farba_stvorca = document.querySelector(".ctverecek");
+  farba_stvorca.style.backgroundColor = "green";
+}
+
+
